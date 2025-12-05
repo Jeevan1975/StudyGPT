0fca0546ab115d6d1ce1840e53b85d18baa9c1f5
Jeevan1975 <jeevang1975@gmail.com>
2025-12-05 23:33:30 +0530

    Refactor document ingestion process and add FastAPI upload endpoint

--- backend/core/ingest.py
+++ backend/core/ingest.py
@@ -11,45 +11,42 @@ load_dotenv()
 
 embeddings = GoogleGenerativeAIEmbeddings(model='gemini-embedding-001')
 
-PERSIST_DIR = Path("../vectorstore")
-DOCUMENTS_DIR = Path("../data/documents")
+BASE_DIR = Path(__file__).resolve().parent.parent
+PERSIST_DIR = BASE_DIR / "vectorstore"
+DOCUMENTS_DIR = BASE_DIR / "data" / "documents"
 
 # Ensure whether directories exist or not
 PERSIST_DIR.mkdir(parents=True, exist_ok=True)
 DOCUMENTS_DIR.mkdir(parents=True, exist_ok=True)
 
 
-def ingest_documents():
-    files = list(DOCUMENTS_DIR.glob("*.pdf"))
+def ingest_documents(file_path: Path):
+    print(f"\nLoading {file_path.name} ...")
     
-    all_docs = []
+    # Load pdf
+    loader = PyPDFLoader(str(file_path))
+    docs = loader.load()
     
-    # Loading documents
-    for file in files:
-        print(f"Loading {file}...")
-        loader = PyPDFLoader(str(file))
-        docs = loader.load()
-        all_docs.extend(docs)
-    
-    # Split documents
+    # Split into chunks
     print("Splitting documents into chunks...")
     splitter = RecursiveCharacterTextSplitter(
         chunk_size=700,
         chunk_overlap=100
     )
-    chunks = splitter.split_documents(all_docs)
+    chunks = splitter.split_documents(docs)
     
     # Store in Chroma
     vector_store = Chroma(
-        collection_name="laptop_manual",
+        collection_name="support_docs",
         embedding_function=embeddings,
         persist_directory=PERSIST_DIR
     )
-    uuids = [str(uuid4()) for _ in range(len(chunks))]
-    vector_store.add_documents(documents=chunks, ids=uuids)
-    print("Ingestion completed, vector store updated")
+    ids = [f"{file_path.stem}_{uuid4()}" for _ in range(len(chunks))]
+    vector_store.add_documents(documents=chunks, ids=ids)
+    print(f"Ingested {file_path.name} successfully.")
     
     
     
 if __name__=="__main__":
-    ingest_documents()
+    for file in DOCUMENTS_DIR.glob("*.pdf"):
+        ingest_documents(file)
